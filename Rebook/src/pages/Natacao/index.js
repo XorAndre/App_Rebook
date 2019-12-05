f7add223c2e682eb5e2afb914a49400483129ec2
Coffee <xor2018@gmail.com>
2019-12-05 12:26:59 -0300

    Upload telas dois

--- Rebook/src/pages/Natacao/index.js
+++ Rebook/src/pages/Natacao/index.js
@@ -39,7 +39,8 @@ import {
   Icon, 
   Input,
   Item,
-  Text 
+  Text, 
+  Row
 } from 'native-base';
 import Professor from '~/assets/img/professor.png';
 
@@ -73,6 +74,66 @@ const openDrawer = () => {
                     <Text style={styles.positionText}>Hora</Text>
                 </Col>
             </Grid>
+            <Grid>
+                <Col>
+                    <View style={styles.Grade}>
+                        <View style={styles.GradeTopo}>
+                            <View style={styles.lineTopo}>
+                                <Text>A</Text>
+                            </View>
+                            <View style={styles.lineTopo}>
+                                <Text>B</Text>
+                            </View>
+                            <View style={styles.lineTopo}>
+                                <Text>C</Text>
+                            </View>
+                            <View style={styles.lineTopo}>
+                                <Text>D</Text>
+                            </View>
+                            <View style={styles.lineTopo}>
+                                <Text>E</Text>
+                            </View>
+                            <View style={styles.lineTopo}>
+                                <Text>F</Text>
+                            </View>
+                        </View>  
+                        <View style={styles.GradeCorpo}>
+                            <Grid style={styles.GradeLinha}>
+                                <Col style={styles.dados}><Text>1</Text></Col>
+                                <Col style={styles.dados}><Text>5</Text></Col>
+                                <Col style={styles.dados}><Text>9</Text></Col>
+                                <Col style={styles.dados}><Text>13</Text></Col>
+                                <Col style={styles.dados}><Text>17</Text></Col>
+                                <Col style={styles.dados}><Text>21</Text></Col>
+                            </Grid>
+                            <Grid style={styles.GradeLinha}>
+                                <Col style={styles.dados}><Text>2</Text></Col>
+                                <Col style={styles.dados}><Text>6</Text></Col>
+                                <Col style={styles.dados}><Text>10</Text></Col>
+                                <Col style={styles.dados}><Text>13</Text></Col>
+                                <Col style={styles.dados}><Text>18</Text></Col>
+                                <Col style={styles.dados}><Text>22</Text></Col>
+                            </Grid>
+                            <Grid style={styles.GradeLinha}>
+                                <Col style={styles.dados}><Text>3</Text></Col>
+                                <Col style={styles.dados}><Text>7</Text></Col>
+                                <Col style={styles.dados}><Text>11</Text></Col>
+                                <Col style={styles.dados}><Text>15</Text></Col>
+                                <Col style={styles.dados}><Text>19</Text></Col>
+                                <Col style={styles.dados}><Text>23</Text></Col>
+                            </Grid>
+                            <Grid style={styles.GradeLinha}>
+                                <Col style={styles.dados}><Text>4</Text></Col>
+                                <Col style={styles.dados}><Text>8</Text></Col>
+                                <Col style={styles.dados}><Text>12</Text></Col>
+                                <Col style={styles.dados}><Text>16</Text></Col>
+                                <Col style={styles.dados}><Text>20</Text></Col>
+                                <Col style={styles.dados}><Text>24</Text></Col>
+                            </Grid>
+                        </View>  
+                    </View> 
+                </Col>
+            </Grid>
         </Content>
     </Container>
   );
@@ -97,6 +158,32 @@ const styles = StyleSheet.create({
         marginTop: 30,
     },
     colProfessor:{
+        alignItems: 'center',
+    },
+    Grade:{
+        borderColor: '#333',
+        marginLeft: 20,
+        width: '90%',
+
+    },
+    GradeTopo:{
+        flexDirection: 'row',
+        marginTop: 20,
+    },
+    lineTopo:{
+        alignItems: 'center',
+        width: '16.6%',
+    },
+    GradeCorpo:{
+        borderStyle: 'solid',
+        borderColor: '#333',
+        borderWidth: 1,
+    },
+    GradeLinha:{
+       alignItems: 'center',
+       padding: 10,
+    },
+    dados:{
         alignItems: 'center',
     }
   });
